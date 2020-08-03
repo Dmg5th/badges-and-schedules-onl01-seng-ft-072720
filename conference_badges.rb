3e1912deaa37f822fa03bf68377dccb1156f5031
Daniel Gould <dmgould310@gmail.com>
2020-08-03 12:23:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-   array.collect do |name|
-     puts "Hello, my name is #{name}."
+  newarr = []
+   array.each do |name|
+     newarr.push("Hello, my name is #{name}.")
    end 
+   newarr
 end 
