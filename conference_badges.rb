b4617517f114b8b8bbbcbcb90c4643608a6154d0
Daniel Gould <dmgould310@gmail.com>
2020-08-03 12:42:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,6 +30,30 @@ end
 
 end 
 
+def batch_badge_creator(array)
+  array.collect do |name|
+   "Hello, my name is #{name}." 
+  end 
+end 
+
+def assign_rooms(array)
+   new_array = []
+   count = 1
+  array.each do |name|
+   new_array.push("Hello, #{name}! You'll be assigned to room #{count}!")
+   count += 1 
+ end 
+ return new_array 
+end 
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id 
+  end 
+  
+  
+end 
+
 
 
 
